a8c71dadd7909d5aafb7cd9efd3b6b186b344d71
Jessica Ayunani <j.ayunani@gmail.com>
2016-03-08 20:34:19 -0800

    Create falsyBouncer.js

---
+++ falsyBouncer.js
@@ -0,0 +1,9 @@
+// Remove all falsy values from an array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+function bouncer(arr) {
+return arr.filter(function(val){
+  return Boolean(val);
+  })
+}
